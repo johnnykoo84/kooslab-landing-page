0929addb70233af7491c0638d7f5591254532539
johnnykoo84 <johnnykoo84@gmail.com>
2023-12-27 17:52:48 +0900

    Update Navbar styling and About page content

--- src/navigation/NavbarTwoColumns.tsx
+++ src/navigation/NavbarTwoColumns.tsx
@@ -7,13 +7,13 @@ type INavbarProps = {
 };
 
 const NavbarTwoColumns = (props: INavbarProps) => (
-  <div className="flex flex-wrap items-center justify-start">
+  <div className="sticky top-0 z-50 flex flex-wrap items-center justify-start border-b-2 border-gray-200 bg-white">
     <div>
       <Link href="/">{props.logo}</Link>
     </div>
 
     <nav className="ml-6 flex items-center">
-      <ul className="navbar font-large flex items-center text-3xl font-bold text-gray-800">
+      <ul className="navbar font-large flex items-center text-3xl text-gray-800">
         <li>
           <Link href="/about">회사 소개</Link>
         </li>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,11 +9,19 @@ import founderProfileSrc from '../assets/ikoo.png';
 import coFounderProfileSrc from '../assets/mandalorian.png';
 import { Logo } from '../templates/Logo';
 
-const Mission = () => (
+type ParagraphProps = {
+  subject: string;
+  texts: string[];
+};
+
+const Paragraph = ({ subject, texts }: ParagraphProps) => (
   <div className="my-10">
-    <h2 className="mb-4 text-3xl">회사의 설립 이유 & 가장 중요한 가치</h2>
-    <p className="text-5xl leading-relaxed">우리는 고객이 필요로 하는 제품을</p>
-    <p className="text-5xl leading-relaxed">만들고 판매합니다</p>
+    <h2 className="mb-4 text-3xl">{subject}:</h2>
+    {texts.map((text, index) => (
+      <p key={index} className="text-5xl leading-relaxed">
+        {text}
+      </p>
+    ))}
   </div>
 );
 
@@ -38,17 +46,17 @@ const AIProfile = () => (
 const FounderIntro = () => (
   <div className="my-20">
     <div>
-      <h2 className="mb-2 text-2xl font-bold">창업팀</h2>
+      <h2 className="mb-2 text-2xl font-bold">멤버 소개</h2>
       <Image
         className="my-8 mr-4 h-24 w-24 rounded-full border-2 border-gray-300"
         src={founderProfileSrc}
         alt="Founder profile image"
       />
       <p className="text-lg font-bold leading-relaxed">구일모</p>
+      <p className="text-lg leading-relaxed">고객 문제 해결 팀</p>
       <p className="text-lg leading-relaxed">
-        B2B SW 제품 세일즈, SW개발, 조직 운영
+        전) 코드스테이츠 CPO, 교육 R&D 그룹리더
       </p>
-      <p className="text-lg leading-relaxed">전) 코드스테이츠 CPO</p>
       <p className="text-lg leading-relaxed">
         전) 벡터코리아 기술영업, 전) 대한항공 시스템 개발, 전) 영텍 하드웨어
         개발
@@ -80,7 +88,16 @@ const About = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />} />
       <div className="mx-auto max-w-7xl px-4 py-6 text-gray-800 sm:px-6 lg:px-8">
-        <Mission />
+        <div className="gap-10">
+          <Paragraph
+            subject="Mission"
+            texts={['고객이 원하는', '제품을', '만들고 판매합니다']}
+          />
+          <Paragraph
+            subject="핵심 가치"
+            texts={['고객 문제 해결', '고객 성공']}
+          />
+        </div>
         <FounderIntro />
       </div>
     </Section>
