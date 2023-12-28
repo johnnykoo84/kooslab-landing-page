f4ce5c427670471f922ea78d5b6ce591955e05c0
johnnykoo84 <johnnykoo84@gmail.com>
2023-12-28 13:04:05 +0900

    Update Hero and VerticalFeatures images

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -11,10 +11,10 @@ const Hero = () => (
         title={
           <>
             {'신규 직원의 즉시 전력화를 위한\n'}
-            <span className="text-primary-500">업무 안내 솔루션</span>
+            <span className="text-primary-500">업무 교육 솔루션</span>
           </>
         }
-        description="사장님의 업무를 대신할 수 있는 업무 안내 솔루션을 제공합니다."
+        description="단기 계약 근로,아르바이트 직원분들을 위한 업무 교육 도구를 제공합니다."
         button={<Button xl>가이드 및 상담 요청하기</Button>}
       />
     </Section>
--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -2,22 +2,26 @@ import { VerticalFeatureRow } from '../feature/VerticalFeatureRow';
 import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
-  <Section
-    title="아직도 신규 직원에게 모든 업무를 직접 알려주고 계신가요?"
-    description="신규 직원 출근 때마다 준비하고, 업무를 직접 알려주고, 또 다시 준비하고, 또 다시 알려주고... 이런 반복적인 업무를 줄이고 싶지 않으신가요?"
-  >
+  <Section>
     <VerticalFeatureRow
-      title="준비된 내용을 순서에 맞게 교육해주세요"
-      description="신규 직원은 업무 방식과 절차를 안내받지 못해 큰 혼란을 겪습니다. 결국 스스로 눈치껏 찾아내어 적응을 해 내지만, 이는 시간과 노력이 많이 듭니다. 모멘텀을 사용하시면, 순서와 절차에 맞게 안내가 필요한 내용을 효과적으로 전달할 수 있습니다."
+      title="아직도 신규 직원에게 모든 업무를 직접 알려주고 계신가요?"
+      description="신규 직원 출근 때마다 준비하고, 업무를 직접 알려주고, 또 다시 준비하고, 또 다시 알려주고... 이런 반복적인 업무를 줄이고 싶지 않으신가요?"
       image="/assets/images/confused.png"
+      imageAlt="First feature alt text"
+      reverse={true}
+    />
+    <VerticalFeatureRow
+      title="준비된 내용을 순서에 맞게 알려주세요"
+      description="신규 직원은 업무 방식과 절차를 안내받지 못해 큰 혼란을 겪습니다. 결국 스스로 눈치껏 찾아내어 적응을 해 내지만, 이는 시간과 노력이 많이 듭니다. 모멘텀을 사용하시면, 순서와 절차에 맞게 안내가 필요한 내용을 효과적으로 전달할 수 있습니다."
+      image="/assets/images/mobile_lms.png"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="짧은 영상기반 교육으로 빠르게 업무를 알려주세요"
+      title="짧은 영상기반 교육으로 업무를 정확하게 알려주세요"
       description="스마트폰으로 찍고 올리면 누구나 쉽게 영상을 만들 수 있습니다. 신규 직원이 출근할 때마다 업무에 필요한 영상을 만들어 두면, 직원은 스스로 업무를 찾아보고, 업무를 수행할 수 있습니다."
       image="/assets/images/video.png"
       imageAlt="Second feature alt text"
-      reverse
+      reverse={true}
     />
   </Section>
 );
