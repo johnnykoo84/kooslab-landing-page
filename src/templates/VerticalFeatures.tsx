9cb520278c8d5fa59d7f1834055e4567320753c6
johnnykoo84 <johnnykoo84@gmail.com>
2023-12-28 11:41:23 +0900

    Update VerticalFeatures component with new titles and descriptions

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,25 +3,19 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="신규 직원분이 빠르게 업무에 적응하고 있나요?"
-    description="직원분이 출근한지 벌써 1주나 지났는데 아직도 알려줘야할 것들이 많아요"
+    title="아직도 신규 직원에게 모든 업무를 직접 알려주고 계신가요?"
+    description="신규 직원 출근 때마다 준비하고, 업무를 직접 알려주고, 또 다시 준비하고, 또 다시 알려주고... 이런 반복적인 업무를 줄이고 싶지 않으신가요?"
   >
     <VerticalFeatureRow
-      title="무엇부터 해야할지 감이 오지 않아요"
-      description="신규 입사자는 회사의 업무 방식과 절차를 안내받지 못해 큰 혼란을 겪습니다. 결국 스스로 눈치껏 찾아내어 적응을 해 내지만, 회사와 개인 모두가 큰 시간 비용을 치루게 됩니다."
-      image="/assets/images/feature.svg"
+      title="준비된 내용을 순서에 맞게 교육해주세요"
+      description="신규 직원은 업무 방식과 절차를 안내받지 못해 큰 혼란을 겪습니다. 결국 스스로 눈치껏 찾아내어 적응을 해 내지만, 이는 시간과 노력이 많이 듭니다. 모멘텀을 사용하시면, 순서와 절차에 맞게 안내가 필요한 내용을 효과적으로 전달할 수 있습니다."
+      image="/assets/images/confused.png"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="일의 핵심이 무엇인지 한참 지나서야 알게 되었어요"
-      description="신규 직원은 현재 목표와 우선순위를 안내받지 못해, 자신의 역할과 책임을 명확히 알기 어렵습니다. 방치된 채로는 결국 시간이 지나도 1인분 만큼의 역할을 해 내기 어렵습니다."
-      image="/assets/images/feature3.svg"
-      imageAlt="Third feature alt text"
-    />
-    <VerticalFeatureRow
-      title="대표님, 사장님이 원하는 기대치를 모르겠어요"
-      description="회사와 신규 직원 모두 구체적인 기대치에 대해 합의된 바가 없기 때문에, 수습 기간 평가는 대충 넘어가게 됩니다."
-      image="/assets/images/feature2.svg"
+      title="짧은 영상기반 교육으로 빠르게 업무를 알려주세요"
+      description="스마트폰으로 찍고 올리면 누구나 쉽게 영상을 만들 수 있습니다. 신규 직원이 출근할 때마다 업무에 필요한 영상을 만들어 두면, 직원은 스스로 업무를 찾아보고, 업무를 수행할 수 있습니다."
+      image="/assets/images/video.png"
       imageAlt="Second feature alt text"
       reverse
     />
